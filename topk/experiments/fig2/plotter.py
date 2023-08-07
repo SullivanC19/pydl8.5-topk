ba9ad10b32bb2c145d96c62d04508b142af9fe5e
SullivanC19 <colinsullivan203@gmail.com>
2023-08-07 14:29:30 -0700

    started plotting fig2

--- topk/experiments/fig2/plotter.py
+++ topk/experiments/fig2/plotter.py
@@ -3,12 +3,14 @@ from datetime import datetime
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from topk.globals import load_latest_results, ALL_DATASETS
 from .constants import K_VALUES, DEPTH_VALUES
 
 
 def main(std=2):
+    sns.set_style()
     figure = plt.figure(layout="constrained", figsize=(16, 12))
     nrows = 6
     ncols = 4
@@ -17,34 +19,37 @@ def main(std=2):
     for i, dataset in enumerate(['artificial-characters']):
         ax = ax_array[i // nrows, i % nrows]
         results = load_latest_results("fig2", dataset)
-        # data = results.groupby(['k', 'depth'])['test_acc', 'timeout'].agg(
-        #     ('test_acc', {'acc': lambda arr: np.mean(arr), 'err': lambda arr: np.sqrt(arr) / np.sqrt(len(arr)) * std}),
-        #     ('timeout', {'timeout': lambda arr: np.any(arr['timeout'])}),
-        # ).reset_index()
 
-        for k in K_VALUES:
-            acc = np.array(results[results['k'] == k].groupby('depth')['test_acc'].agg('mean'))
-            print(acc)
-            exit()
-            err = np.array(grouped.agg('std')) / np.sqrt(len(acc)) * std
-            marker = "*" if k == 0 else 'o'
-            k = 'd' if k > 0 else str(k)
-            plt.errorbar(
-                DEPTH_VALUES[~data['timeout']],
-                data['acc'][~data['timeout']],
-                yerr=data['err'],
-                marker=marker,
-                fillstyle='full',
-                keyword=f"Top-{k}",
-            )
-            plt.errorbar(
-                DEPTH_VALUES[data['timeout']],
-                data['acc'][data['timeout']],
-                yerr=data['err'],
-                marker=marker,
-                fillstyle='none',
-                keyword=f"Top-{k}",
-            )
+        sns.lineplot(x='depth', y='test_acc', estimator='mean')
+
+        # data = results.groupby(['k', 'depth'])['test_acc'].agg(
+        #     acc=lambda arr: np.mean(arr), 'err':
+        #         lambda arr: np.sqrt(arr) / np.sqrt(len(arr)) * std}),
+        # ).reset_index()
+        #
+        # for k in K_VALUES:
+        #     acc = np.array(results[results['k'] == k].groupby('depth')['test_acc'].agg('mean'))
+        #     print(acc)
+        #     exit()
+        #     err = np.array(grouped.agg('std')) / np.sqrt(len(acc)) * std
+        #     marker = "*" if k == 0 else 'o'
+        #     k = 'd' if k > 0 else str(k)
+        #     plt.errorbar(
+        #         DEPTH_VALUES[~data['timeout']],
+        #         data['acc'][~data['timeout']],
+        #         yerr=data['err'],
+        #         marker=marker,
+        #         fillstyle='full',
+        #         keyword=f"Top-{k}",
+        #     )
+        #     plt.errorbar(
+        #         DEPTH_VALUES[data['timeout']],
+        #         data['acc'][data['timeout']],
+        #         yerr=data['err'],
+        #         marker=marker,
+        #         fillstyle='none',
+        #         keyword=f"Top-{k}",
+        #     )
 
 
 
